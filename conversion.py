7581b71ab2b021d9bbcfdd61db10efaf2a7959a6
The1Alain <alainchidiac23@gmail.com>
2026-06-06 15:14:09 +0400

    Refactor conversion functions and add language choice

--- conversion.py
+++ conversion.py
@@ -1,134 +1,185 @@
 # Code fait par Alain Chidiac
+# Choix de la langue et de la convertion
+# Language and conversion choice
+langue = input('0 -> English 1  -> Français')
+if langue == '1':
+    langue = "French"
+else :
+    langue = "English"
+if langue == "French":
+    conve = input("0  -> Décimale à binaire | 1 -> Décimale à hexadécimale | 2 -> Héxadécimale à binaire | 3 -> Binaire à décimale | 4 -> Héxadécimale à décimale | 5 -> Binaire à Héxadécimale")
+elif langue == "English":
+    conve = input("0  -> Decimal to binary | 1 -> Decimal to hexadecimal | 2 -> Hexadecimal to binary | 3 -> Binary to decimal | 4 -> Hexadecimal to decimal | 5 -> Binary to Hexadecimal")
+run = True
 # Conversion d'un nombre à base 10 à un binaire
 # Convertion of a decimal into a binary
-'''bin = ""
-dec = int(input("Met le nombre en base 10 : "))
-while dec >= 1 :
-    modulo = dec % 2
-    dec = dec // 2
-    if modulo == 1 :
-        bin = "1" + bin
+def dec_bin():
+    bin = ""
+    if langue == "French":
+        dec = int(input("Met le nombre en base 10 : "))
     else :
-        bin = "0" + bin
-print(bin)'''
+        dec = int(input("Put the decimal number : "))
+    while dec >= 1 :
+        modulo = dec % 2
+        dec = dec // 2
+        if modulo == 1 :
+            bin = "1" + bin
+        else :
+            bin = "0" + bin
+    print(bin)
 # Conversion d'un nombre en base 10 à un hexadécimale
 # Convertion of a decimal into an hexadecimal
-"""sup = ("A","B","C","D","E","F") # We define the possible values that are over nine
-hex = ""
-dec = int(input("Met le nombre en base 10 : "))
-while dec >= 1 :
-    modulo = dec % 16
-    dec = dec // 16
-    if modulo != 0:
-        if modulo < 10 :
-            hex = str(modulo) + hex
-        else :
-            n = 10
-            n_tuple = 0
-            while n != modulo :
-                n += 1
-                n_tuple += 1
-            fin = sup[n_tuple]
-            hex = str(fin) + hex
+def dec_hex():
+    sup = ("A","B","C","D","E","F") # We define the possible values that are over nine
+    hex = ""
+    if langue == "French":
+        dec = int(input("Met le nombre en base 10 : "))
     else :
-        hex = str(dec) + hex
-print(hex)"""
+        dec = int(input("Put the decimal number : "))
+    while dec >= 1 :
+        modulo = dec % 16
+        dec = dec // 16
+        if modulo != 0:
+            if modulo < 10 :
+                hex = str(modulo) + hex
+            else :
+                n = 10
+                n_tuple = 0
+                while n != modulo :
+                    n += 1
+                    n_tuple += 1
+                fin = sup[n_tuple]
+                hex = str(fin) + hex
+        else :
+            hex = str(dec) + hex
+    print(hex)
 # Conversion d'un nombre hexadécimale à un binaire
 # Conversion of an hexadecimal into a binary
-"""hex = input("met un nombre en base 16 : ")
-sup = ("A","B","C","D","E","F")
-bin = ""
-for t in hex :
-    try :
-        t = int(t)
-        bin_2 = ""
-        while t >= 1 :
-            modulo = t % 2
-            t = t // 2
-            if modulo == 1 :
-                bin_2 = "1" + bin_2
-            else :
+def hex_bin():
+    if langue == "French":
+        hex = input("Met un nombre en base 16 : ")
+    else :
+        hex = input("Put the hexadecimal number : ")
+    sup = ("A","B","C","D","E","F")
+    bin = ""
+    for t in hex :
+        try :
+            t = int(t)
+            bin_2 = ""
+            while t >= 1 :
+                modulo = t % 2
+                t = t // 2
+                if modulo == 1 :
+                    bin_2 = "1" + bin_2
+                else :
+                    bin_2 = "0" + bin_2
+            while len(bin_2) < 4 :
                 bin_2 = "0" + bin_2
-        while len(bin_2) < 4 :
-            bin_2 = "0" + bin_2
-        bin = bin + bin_2 + " "
-    except :
-        n = 0
-        bin_3 = ""
-        while sup[n] != t :
-            n += 0
-        dec = 10 + n
-        while dec >= 1 :
-            modulo = dec % 2
-            dec = dec // 2
-            if modulo == 1 :
-                bin_3 = "1" + bin_3
-            else :
-                bin_3 = "0" + bin_3
-        bin = bin + bin_3 + " "
-print(bin)"""
+            bin = bin + bin_2 + " "
+        except :
+            n = 0
+            bin_3 = ""
+            while sup[n] != t :
+                n += 0
+            dec = 10 + n
+            while dec >= 1 :
+                modulo = dec % 2
+                dec = dec // 2
+                if modulo == 1 :
+                    bin_3 = "1" + bin_3
+                else :
+                    bin_3 = "0" + bin_3
+            bin = bin + bin_3 + " "
+    print(bin)
 # Conversion d'un nombre binaire à la base 10
 # Convertion of a binary into a decimal
-"""dec = 0
-bin = input("Met le nombre binaire : ")
-puis = len(bin) - 1
-for t in bin :
-    bin = int(bin)
-    if int(t) > 1 :
-        print("Le nombre n'est pas binaire.")
-    num = int(t)*2**puis
-    dec += num
-    puis -= 1
-    bin = str(bin)
-print(dec)"""
-# Conversion d'un nombre hexadécimal à la base 10
-# COnvertion of a hexadecimal into a decimal
-"""dec = 0
-sup = ("A","B","C","D","E","F")
-hex = input("Met le nombre en base 16 : ")
-puis = len(hex) - 1
-for t in hex :
-    try :
-        num = int(t)*16**puis
-    except :
-        num = 0
-        n = 0
-        diz = 10
-        while sup[n] != t :
-            n += 0
-            diz += n
-        num = int(diz)*16**puis
-    puis -= 1
-    dec += num
-print(dec)"""
-# Conversion d'un nombre binaire à un hexadécimale
-# Convertion of a binary into an hexadecimal
-"""sup = ("F","E","D","C","B","A")
-hex = ""
-n_bit = 0
-bin = input("Met le nombre binaire : ")
-for t in bin :
-    n_bit += 1
-if n_bit % 4 != 0 :
-    n_0 = 4 - n_bit % 4
-    for i in range(n_0) :
-        bin = "0" + bin
-for t in range (0, len(bin), 4) :
-    bits = bin[t : t+4]
-    a, b, c, d = bits
-    bin_temp = a + b + c + d
+def bin_dec():
     dec = 0
-    puis = len(bin_temp) - 1
-    for t in bin_temp :
+    if langue == "French":
+        bin = input("Met le nombre binaire : ")
+    else :
+        bin = input("Put the binary number : ")
+    puis = len(bin) - 1
+    for t in bin :
+        bin = int(bin)
         if int(t) > 1 :
             print("Le nombre n'est pas binaire.")
         num = int(t)*2**puis
         dec += num
         puis -= 1
-        bin_temp = str(bin_temp)
-    if dec < 10 :
-        hex = hex + str(dec)
+        bin = str(bin)
+    print(dec)
+# Conversion d'un nombre hexadécimal à la base 10
+# Convertion of a hexadecimal into a decimal
+def hex_dec():
+    dec = 0
+    sup = ("A","B","C","D","E","F")
+    if langue == "French":
+        hex = input("Met le nombre en base 16 : ")
     else :
-        let = 15 - dec
-        hex = hex + sup[let]
-print(hex)"""
+        hex = input("Put the hexadecimal number : ")
+    puis = len(hex) - 1
+    for t in hex :
+        try :
+            num = int(t)*16**puis
+        except :
+            num = 0
+            n = 0
+            diz = 10
+            while sup[n] != t :
+                n += 0
+                diz += n
+            num = int(diz)*16**puis
+        puis -= 1
+        dec += num
+    print(dec)
+# Conversion d'un nombre binaire à un hexadécimale
+# Convertion of a binary into an hexadecimal
+def bin_hex():
+    sup = ("F","E","D","C","B","A")
+    hex = ""
+    n_bit = 0
+    if langue == "French":
+        bin = input("Met le nombre binaire : ")
+    else :
+        bin = input("Put the binary number : ")
+    for t in bin :
+        n_bit += 1
+    if n_bit % 4 != 0 :
+        n_0 = 4 - n_bit % 4
+        for i in range(n_0) :
+            bin = "0" + bin
+    for t in range (0, len(bin), 4) :
+        bits = bin[t : t+4]
+        a, b, c, d = bits
+        bin_temp = a + b + c + d
+        dec = 0
+        puis = len(bin_temp) - 1
+        for t in bin_temp :
+            if int(t) > 1 :
+                print("Le nombre n'est pas binaire.")
+            num = int(t)*2**puis
+            dec += num
+            puis -= 1
+            bin_temp = str(bin_temp)
+        if dec < 10 :
+            hex = hex + str(dec)
+        else :
+            let = 15 - dec
+            hex = hex + sup[let]
+    print(hex)
+def conv():
+    if conve == '0':
+        dec_bin()
+    elif conve == '1':
+        dec_hex()
+    elif conve == '2':
+        hex_bin()
+    elif conve == '3':
+        bin_dec()
+    elif conve == '4':
+        hex_dec()
+    elif conve == '5':
+        bin_hex()
+while run == True :
+    conv()
